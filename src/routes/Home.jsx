f92c08a72f9b3aa3ead78126f45497a3d45def46
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-06 12:33:25 -0500

    add tailwind to project

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex h-vh justify-center items-center h-screen">
+      <div className="text-xl text-center">
+        <h2>Pages:</h2>
+        <ul>
+          <Link to="/">404 Not Found</Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
